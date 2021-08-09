a14f354266212e00bf2ab25ec407f48b8eb72fde
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2021-08-09 20:13:23 +0530

    create demopub.java
    it is extension of MainStudent

---
+++ DemoPub.java
@@ -0,0 +1,7 @@
+public class DemoPub {
+   public static void main(String[] args) {
+       Student Rahul=new Student();
+       Rahul.getData("Rahul Singh", "Dehradun", 3.78);
+       Rahul.printData();
+   } 
+}
